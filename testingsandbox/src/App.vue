a14810765af9a4d7eedbfc9e0ff406efa3e92c2c
Terry <terry.lay@macegroup.com>
2022-01-21 12:33:34 +1100

    Update

--- testingsandbox/src/App.vue
+++ testingsandbox/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Test</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
